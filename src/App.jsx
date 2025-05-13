b435a976f08d658f11c08b99dd5943a6c1297133
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-05-13 17:42:04 +0200

    [UPD] eentrambe le liste

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,14 @@
-import { useState } from 'react';
-import axios from 'axios';
+
+import Card from './components/Card';
 import './App.css';
 
 
 function App() {
-  const [actresses, setActresses] = useState([]);
-
-  const fetchActresses = () => {
-    axios
-      .get("https://lanciweb.github.io/demo/api/actresses/")
-      .then((response) => { setActresses(response.data) })
-      .catch((error) => { console.error('ERRORE CARICAMENTO PERSONAGGI', error); });
-  }
-
-  const [actors, setActors] = useState([]);
-
-  const fetchActors = () => {
-    axios
-      .get("https://lanciweb.github.io/demo/api/actors/")
-      .then((response) => { setActors(response.data) })
-      .catch((error) => { console.error('ERRORE CARICAMENTO PERSONAGGI', error); });
-  }
-
-
-
-  return ( 
-    <div className='row'>
-      <div className='col'>
-        <div className='container'>
-          <button onClick={fetchActresses}>Carica Attrici</button>
-          <ul className="card">
-            {actresses.map((actress) => {
-              return (
-                <li key={actress.id}>
-                  <h2>{actress.name}</h2>
-                  <img src={actress.image} alt={actress.name} />
-                  <p>{actress.nationality}</p>
-                  <p>{actress.most_famous_movies}</p>
-                  <p>{actress.birth_year}</p>
-                  <p>{actress.awards}</p>
-                  <p>{actress.biography}</p>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-      </div>
-      <div className='col'>
-        <div className='container'>
-          <button onClick={fetchActors}>Carica Attori</button>
-          <ul className="card">
-            {actors.map((actors) => {
-              return (
-                <li key={actors.id}>
-                  <h2>{actors.name}</h2>
-                  <img src={actors.image} alt={actors.name} />
-                  <p>{actors.nationality}</p>
-                  <p>{actors.known_for}</p>
-                  <p>{actors.birth_year}</p>
-                  <p>{actors.awards}</p>
-                  <p>{actors.biography}</p>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </div>  
-     </div>
+  return (
+    <div>
+      <h1>Attori e Attrici</h1>
+      <Card />
+    </div>
   );
 };
 
