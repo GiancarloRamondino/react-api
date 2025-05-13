a1b7becca44d9884c4e6d6ab23ff7482a04fe23a
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-05-13 17:22:25 +0200

    [UPD] oganized css

--- src/App.jsx
+++ src/App.jsx
@@ -24,45 +24,49 @@ function App() {
 
 
 
-  return (
-    <div>
-      <div>
-        <button onClick={fetchActresses}>Carica Attrici</button>
-        <ul>
-          {actresses.map((actress) => {
-            return (
-              <li key={actress.id}>
-                <h2>{actress.name}</h2>
-                <img src={actress.image} alt={actress.name} />
-                <p>{actress.nationality}</p>
-                <p>{actress.most_famous_movies}</p>
-                <p>{actress.birth_year}</p>
-                <p>{actress.awards}</p>
-                <p>{actress.biography}</p>
-              </li>
-            )
-          })}
-        </ul>
+  return ( 
+    <div className='row'>
+      <div className='col'>
+        <div className='container'>
+          <button onClick={fetchActresses}>Carica Attrici</button>
+          <ul className="card">
+            {actresses.map((actress) => {
+              return (
+                <li key={actress.id}>
+                  <h2>{actress.name}</h2>
+                  <img src={actress.image} alt={actress.name} />
+                  <p>{actress.nationality}</p>
+                  <p>{actress.most_famous_movies}</p>
+                  <p>{actress.birth_year}</p>
+                  <p>{actress.awards}</p>
+                  <p>{actress.biography}</p>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </div>
-      <div>
-        <button onClick={fetchActors}>Carica Attori</button>
-        <ul>
-          {actors.map((actors) => {
-            return (
-              <li key={actors.id}>
-                <h2>{actors.name}</h2>
-                <img src={actors.image} alt={actors.name} />
-                <p>{actors.nationality}</p>
-                <p>{actors.known_for}</p>
-                <p>{actors.birth_year}</p>
-                <p>{actors.awards}</p>
-                <p>{actors.biography}</p>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
+      <div className='col'>
+        <div className='container'>
+          <button onClick={fetchActors}>Carica Attori</button>
+          <ul className="card">
+            {actors.map((actors) => {
+              return (
+                <li key={actors.id}>
+                  <h2>{actors.name}</h2>
+                  <img src={actors.image} alt={actors.name} />
+                  <p>{actors.nationality}</p>
+                  <p>{actors.known_for}</p>
+                  <p>{actors.birth_year}</p>
+                  <p>{actors.awards}</p>
+                  <p>{actors.biography}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>  
+     </div>
   );
 };
 
